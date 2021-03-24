b4ffc0536490451e755859d2852337cf6258c6cf
M.Ihsan Fadilah <61552754+isanbayo@users.noreply.github.com>
2021-03-24 12:47:03 +0700

    sunmori
    ampun guru
    
    Co-Authored-By: M.Ihsan Fadilah <61552754+isanbayo@users.noreply.github.com>

--- routes/main.js
+++ routes/main.js
@@ -11,14 +11,22 @@ router.get('/api', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
+router.get('/api/about', (req, res) => {
+    res.sendFile(__path + '/views/about.html')
+})
+
+router.get('/api/news', (req, res) => {
+    res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
+})
+
 router.get('/config', (req, res) => {
     config = {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'sanbot',
+            namaowner: 'ihsan',
+            instagram: 'isanbayo',
             youtube : 'Gak Punya'
         }
     }
